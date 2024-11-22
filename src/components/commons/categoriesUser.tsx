f97ca6dbfc3b5fc2c902f430ed5ff8813db8c445
= <=>
2024-11-22 17:29:12 +0700

    update

--- src/components/commons/categoriesUser.tsx
+++ src/components/commons/categoriesUser.tsx
@@ -12,8 +12,8 @@ import {
   faPercent,
   faCog,
   faQuestionCircle,
-} from '@fortawesome/free-solid-svg-icons';
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+} from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
 export interface UserCategoryItem {
   name: string;
@@ -29,41 +29,50 @@ export interface UserCategorySection {
 
 export const userCategories: UserCategorySection[] = [
   {
-    section: 'Quản lý đơn hàng',
+    section: "Quản lý đơn hàng",
     items: [
-      { name: 'Đơn mua', href: '/don-mua', icon: faBagShopping },
-      { name: 'Đơn bán', href: '/don-ban', icon: faReceipt },
+      { name: "Đơn mua", href: "/don-mua", icon: faBagShopping },
+      { name: "Đơn bán", href: "/don-ban", icon: faReceipt },
     ],
   },
   {
-    section: 'Tiện ích',
+    section: "Tiện ích",
     items: [
-      { name: 'Tin đăng đã lưu', href: '/tin-luu', icon: faHeart },
-      { name: 'Tìm kiếm đã lưu', href: '/tim-kiem-luu', icon: faSearch },
-      { name: 'Đánh giá từ tôi', href: '/danh-gia', icon: faStar },
+      { name: "Tin đăng đã lưu", href: "/tin-luu", icon: faHeart },
+      { name: "Tìm kiếm đã lưu", href: "/tim-kiem-luu", icon: faSearch },
+      { name: "Đánh giá từ tôi", href: "/danh-gia", icon: faStar },
     ],
   },
   {
-    section: 'Dịch vụ trả phí',
+    section: "Dịch vụ trả phí",
     items: [
-      { name: 'Đồng Tốt', href: '/dong-tot', icon: faCoins },
-      { name: 'Gói PRO', href: '/goi-pro', icon: faAward, tag: 'Mới' },
-      { name: 'Lịch sử giao dịch', href: '/lich-su-giao-dich', icon: faHistory },
-      { name: 'Cửa hàng / chuyên trang', href: '/cua-hang', icon: faStore, tag: 'Tạo ngay' },
+      { name: "Đồng Tốt", href: "/dong-tot", icon: faCoins },
+      { name: "Gói PRO", href: "/goi-pro", icon: faAward, tag: "Mới" },
+      {
+        name: "Lịch sử giao dịch",
+        href: "/lich-su-giao-dich",
+        icon: faHistory,
+      },
+      {
+        name: "Cửa hàng / chuyên trang",
+        href: "/cua-hang",
+        icon: faStore,
+        tag: "Tạo ngay",
+      },
     ],
   },
   {
-    section: 'Ưu đãi, khuyến mãi',
+    section: "Ưu đãi, khuyến mãi",
     items: [
-      { name: 'Chợ Tốt ưu đãi', href: '/uu-dai', icon: faGift },
-      { name: 'Ưu đãi của tôi', href: '/uu-dai-cua-toi', icon: faPercent },
+      { name: "Chợ Tốt ưu đãi", href: "/uu-dai", icon: faGift },
+      { name: "Ưu đãi của tôi", href: "/uu-dai-cua-toi", icon: faPercent },
     ],
   },
   {
-    section: 'Khác',
+    section: "Khác",
     items: [
-      { name: 'Cài đặt tài khoản', href: '/cai-dat', icon: faCog },
-      { name: 'Trợ giúp', href: '/tro-giup', icon: faQuestionCircle },
+      { name: "Cài đặt tài khoản", href: "/cai-dat", icon: faCog },
+      { name: "Trợ giúp", href: "/tro-giup", icon: faQuestionCircle },
     ],
   },
 ];
