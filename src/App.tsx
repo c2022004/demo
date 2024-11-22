c7a4ac6e9db7a1960a1a2059ec5eb4d280e95a63
Ngô Tấn Cang <156807865+CangNgo@users.noreply.github.com>
2024-11-22 22:12:54 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,3 @@
-/*import { Link, Outlet } from "react-router-dom";
-import Login from "./pages/LoginPage";
-function Header() {
-  const page = [
-    { path: "/trang-chu", name: "Trang chủ" },
-    { path: "/dang-nhap", name: "Đăng nhập" },
-    { path: "/dang-ky", name: "Đăng ký" },
-  ];
-  return (
-    <div className="w-full ">
-      {page.map((item) => (
-        <Link to={item.path} children={item.name} />
-      ))}
-      <Outlet />
-    </div>
-  );
-}
-
-export default Header;*/
-
 import React from 'react';
 import Header from './components/layouts/Header'; // Đường dẫn đến file `Header`
 
